7eed9211d0f2574e9627a948eefdea7c7919f7d3
cheenxuan <cheenxuan2216@gmail.com>
2024-06-13 14:09:04 +0800

    remove other project

--- imagedemo/android/app/src/main/kotlin/com/example/imagedemo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.imagedemo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- textiamge_demo/android/app/src/main/kotlin/com/example/textiamge_demo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.textiamge_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

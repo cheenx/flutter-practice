2202706e1ac899f07441cae9dfe033c7ec09449b
cheenxuan <cheenxuan2216@gmail.com>
2024-06-27 14:42:21 +0800

    init flutter_redux_proj

---
+++ flutter_redux_practice/android/app/src/main/kotlin/me/cheenx/flutter_redux_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package me.cheenx.flutter_redux_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ gsy_github_app/lib/test_redux.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart' show Store;
+
+enum Actions { Increment }
+
+int counterReducer(int state, dynamic action) {
+  return action == Actions.Increment ? state + 1 : state;
+}
+
+void main() {
+  final store = Store<int>(counterReducer, initialState: 0);
+  runApp(FlutterReduxApp(
+    title: "Flutter Redux Demo",
+    store: store,
+  ));
+}
+
+class FlutterReduxApp extends StatelessWidget {
+  final Store<int>? store;
+  final String? title;
+  const FlutterReduxApp({super.key, this.store, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    /// StoreProvider 应封装您的 MaterialApp 或 WidgetsApp。这将确保所有路由都能访问store。
+    return StoreProvider<int>(
+        store: store!,
+        child: MaterialApp(
+          theme: ThemeData.dark(),
+          title: title!,
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text(title!),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StoreConnector<int, String>(
+                    converter: (store) => store.state.toString(),
+                    builder: (context, count) {
+                      return Text(
+                        'The button has been pushed this many times: $count',
+                        style: Theme.of(context).textTheme.displaySmall,
+                      );
+                    },
+                  )
+                ],
+              ),
+            ),
+            floatingActionButton: StoreConnector<int, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(Actions.Increment);
+              },
+              builder: (context, callback) {
+                return FloatingActionButton(
+                  onPressed: callback,
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                );
+              },
+            ),
+          ),
+        ));
+  }
+}
